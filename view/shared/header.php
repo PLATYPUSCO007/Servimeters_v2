688d1ee7ce07a3b2a51861c8442b30d03aeee37c
Esteban Serna <131990358+sm-dev-esteban@users.noreply.github.com>
2023-05-09 20:07:33 -0500

    Add files via upload

--- view/shared/header.php
+++ view/shared/header.php
@@ -8,17 +8,17 @@
         <title>Servimeters</title>
         <link rel="icon" href="../images/Logo Prueba SM.png" type="image/*" sizes="16x16">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-        <link rel="stylesheet" href="../node_modules/admin-lte/plugins/fontawesome-free/css/all.min.css">
+        <link rel="stylesheet" href="../AdminLTE/plugins/fontawesome-free/css/all.min.css">
         <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-        <link rel="stylesheet" href="../node_modules/admin-lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-        <link rel="stylesheet" href="../node_modules/admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-        <link rel="stylesheet" href="../node_modules/admin-lte/plugins/jqvmap/jqvmap.min.css">
-        <link rel="stylesheet" href="../node_modules/admin-lte/dist/css/adminlte.min.css">
-        <link rel="stylesheet" href="../node_modules/admin-lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-        <link rel="stylesheet" href="../node_modules/admin-lte/plugins/daterangepicker/daterangepicker.css">
-        <link rel="stylesheet" href="../node_modules/admin-lte/plugins/summernote/summernote-bs4.min.css">
-        <link rel="stylesheet" href="../node_modules/admin-lte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-        <link rel="stylesheet" href="../node_modules/admin-lte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-        <link rel="stylesheet" href="../node_modules/admin-lte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
-        <link rel="stylesheet" href="../node_modules/admin-lte/plugins/sweetalert2/sweetalert2.min.css">
+        <link rel="stylesheet" href="../AdminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+        <link rel="stylesheet" href="../AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+        <link rel="stylesheet" href="../AdminLTE/plugins/jqvmap/jqvmap.min.css">
+        <link rel="stylesheet" href="../AdminLTE/dist/css/adminlte.min.css">
+        <link rel="stylesheet" href="../AdminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+        <link rel="stylesheet" href="../AdminLTE/plugins/daterangepicker/daterangepicker.css">
+        <link rel="stylesheet" href="../AdminLTE/plugins/summernote/summernote-bs4.min.css">
+        <link rel="stylesheet" href="../AdminLTE/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+        <link rel="stylesheet" href="../AdminLTE/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+        <link rel="stylesheet" href="../AdminLTE/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+        <link rel="stylesheet" href="../AdminLTE/plugins/sweetalert2/sweetalert2.min.css">
     </head>
